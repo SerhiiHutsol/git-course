11e1e17148edcdc866e694130fc29c2bbcbf5125
Serhii_Hutsol <waq2120@gmail.com>
2022-10-29 17:59:32 +0200

    commit 1

---
+++ test.py
@@ -0,0 +1,8 @@
+print('hello world')
+print('hello world h2')
+
+def res(a,b):
+    daff = 12 
+
+def res2():
+    result = 13
---
+++ test_new.py
@@ -0,0 +1,8 @@
+
+a=12
+b=10
+def print_ans(a,b):
+    res = a+b 
+    return res
+
+print(print_ans(a,b))
